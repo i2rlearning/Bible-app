4828b25eb6f4433327ab80dd0eccd85f7c87a46a
i2rlearning <162467907+i2rlearning@users.noreply.github.com>
2025-10-16 06:40:15 +0300

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,6 @@
 const Size = Quill.import('attributors/style/size');
 Size.whitelist = ['8px','10px','12px','14px','16px','18px','24px','32px'];
-Quill.register({ 'formats/size': Size }, true);   // <-- key change
+Quill.register({ 'formats/size': Size }, true);
 
 const toolbarOptions = [
   [{ size: Size.whitelist }],   // numeric dropdown
